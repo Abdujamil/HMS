acc4f6d89cc9c931fb42877fd446b199e452af2b
Abdujamil <suhrobgafarov3887@gmail.com>
2022-01-19 09:50:34 +0500

    Добавьте файлы проекта.

---
+++ HMS/App_Start/Startup.Auth.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using HMS.Models;
+using HMS.Data;
+using HMS.Entities;
+using HMS.Services;
+
+namespace HMS
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            
+            app.CreatePerOwinContext(HMSContext.Create);
+            app.CreatePerOwinContext<HMSUserManager>(HMSUserManager.Create);
+            app.CreatePerOwinContext<HMSSignInManager>(HMSSignInManager.Create);
+            app.CreatePerOwinContext<HMSRoleManager>(HMSRoleManager.Create);
+
+           
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<HMSUserManager, HMSUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+           
+        }
+    }
+}
---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Ural International Resort";
+        public const string HotelShortName = "Ural";
+        public const string HotelNameAbbreviate = "SIR";
+    }
+}
